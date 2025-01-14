0886814bb5ddebaffcb8389b167ffcf0a92d4e0e
김덕용 <yh100112@naver.com>
2025-01-14 22:58:20 +0900

    init commit

---
+++ Server/GameServer/ServerPacketHandler.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "ServerPacketHandler.h"
+#include "BufferReader.h"
+#include "BufferWriter.h"
+
+PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len)
+{
+	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+	// TODO : Log
+	return false;
+}
+
+bool Handle_C_LOGIN(PacketSessionRef& session, Protocol::C_LOGIN& pkt)
+{
+	return true;
+}
+
+bool Handle_C_ENTER_GAME(PacketSessionRef& session, Protocol::C_ENTER_GAME& pkt)
+{
+	return true;
+}
+
+bool Handle_C_CHAT(PacketSessionRef& session, Protocol::C_CHAT& pkt)
+{
+	return true;
+}
---
+++ Server/GameServer/pch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+#ifdef _DEBUG
+#pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
+#else
+#pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
+#pragma comment(lib, "Protobuf\\Release\\libprotobuf.lib")
+#endif
+
+#include "CorePch.h"
+
+#include "Protocol.pb.h"
+#include "Enum.pb.h"
+#include "Struct.pb.h"
+#include "ServerPacketHandler.h"
+
+using GameSessionRef = shared_ptr<class GameSession>;
